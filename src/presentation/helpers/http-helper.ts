599fe1afceedaa3574551534d4d9d20a1ff54f12
Gabriel Marioto <camilomarioto@hotmail.com>
2021-10-16 22:37:25 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,7 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { IHttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
 
 export const badRequest = (error: Error): IHttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 })
